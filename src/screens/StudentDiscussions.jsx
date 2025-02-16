875405e531a70022a94d1b45c05d65149053d911
bsc-com-32-20 <bsc-com-32-20@unima.ac.mw>
2025-02-16 17:06:17 +0200

    Modified the discussions page a bit

--- src/screens/StudentDiscussions.jsx
+++ src/screens/StudentDiscussions.jsx
@@ -1,15 +1,19 @@
 import { useState } from "react";
-import StudentSideBar from "../components/Student/SideBar";
+import StudentSideBar2 from "../components/Student/SideBar2";
+import { AiOutlineCalendar, AiOutlineClockCircle } from "react-icons/ai";
+
 
 export default function StudentDiscussions() {
     const today = new Date();
     const date = today.toLocaleDateString();
-    const time = today.getTime();
+    const time = today.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+
 
     const discussions = [
         { name: "Performance Optimization in Mobile Apps", date: date, time: time, activity: "Pending" },
         {name: "Performance Optimization in Mobile Apps", date: date, time: time, activity:"Pending" },
         {name: "Performance Optimization in Mobile Apps", date: date, time: time, activity:"Done" },
+        {name: "Performance Optimization in Mobile Apps", date: date, time: time, activity:"Active" },
         {name: "Performance Optimization in Mobile Apps", date: date, time: time, activity:"Active" },
         
     ];
@@ -19,22 +23,22 @@ export default function StudentDiscussions() {
 
     return (
         <div className="flex flex-row gap-5">
-            <StudentSideBar />
-            <section className="ml-[17%] w-[100%] mb-20">
-                <div className="bg-white w-[97%] h-[95%] mt-9 mr-auto rounded-3xl ">
+            <StudentSideBar2 />
+            <section className="ml-[17%] w-[100%] mb-20 ">
+                <div className="bg-white w-[97%] h-[95%] mt-9 mr-auto rounded-3xl pb-10">
                     <h1 className="text-sky-900 text-xl font-semibold ml-10 pt-2">Discussions</h1>
                     <div className="grid grid-cols-1 gap-2 mt-2 grid-row-4">
                         {
                             (showAll ? discussions : discussions.slice(0, 4)).map((discussion, index) => (
-                                <div key={index} className="bg-white rounded-md overflow-hidden w-[85%] ml-10 mr-auto flex flex-col border-gray-300 border-2 pt-2 pb-4">
+                                <div key={index} className="bg-white rounded-md overflow-hidden w-[85%] ml-10 mr-auto flex flex-col border-gray-300 border-2 pt-2 pb-6">
                                     <h2 className="mr-auto ml-4">{discussion.name}</h2>
-                                    <div className="grid grid-cols-2 flex flex-col">
-                                        <div className="grid grid-cols-2 gap-1 mt-3">
-                                            <h5 className="ml-4">Date: {discussion.date}</h5>
-                                            <h5>Time: {discussion.time}</h5>
+                                    <div className="grid grid-cols-2-lg grid-rows-2-sm flex flex-col-lg flex-row-sm">
+                                        <div className="grid grid-cols-2 mt-3">
+                                            <h5 className="ml-4 flex flex-1 gap-2"><AiOutlineCalendar className="text-[1.5rem] text-blue-600" /> {discussion.date}</h5>
+                                            <h5 className="flex flex-1 gap-2"><AiOutlineClockCircle className="text-[1.5rem] text-blue-600"/> {discussion.time}</h5>
                                         </div>
                                         <span
-                                            className={`ml-auto mr-6 border-2 rounded-3xl pl-4 pr-4 pt-1 pb-1 align-middle ${
+                                            className={`ml-auto mr-6 border-2 rounded-3xl pl-4 pr-4 pb-auto align-middle ${
                                                 discussion.activity === "Pending"
                                                     ? "border-blue-300 bg-blue-100 text-blue-500"
                                                     : discussion.activity === "Done"
