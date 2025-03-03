a1a8c3e1dc82a72d762f598c10ffe60871a6ebdf
Angellah khumula <bsc-com-22-20@unima.ac.mw>
2025-03-03 14:58:07 +0200

    modified Navbar

--- src/components/HomeNavBar.jsx
+++ src/components/HomeNavBar.jsx
@@ -5,7 +5,7 @@ export default function HomeNavBar() {
 
         <div className="hidden lg:flex lg:flex-row lg:items-center lg:justify-end bg-white lg:space-x-5 lg:text-lg lg:text-white lg:h-[10vh] lg:w-full lg:mr-10 lg:ml-auto">
                         <span className="lg:text-4xl text-sky-900 font-bold mr-auto px-5">Educonnect</span>
-                        {/* <NavLink to="/" className={({isActive}) => isActive ? "hover:bg-sky-900 bg-sky-700 font-medium h-full p-1 hover:rounded-lg" : "hover:bg-sky-400 font-medium p-1 rounded-lg"}>Home</NavLink> */}
+                        
                         <NavLink to="/Abous Us" className={({isActive}) => isActive ? "hover:bg-sky-700 bg-red-700 font-medium h-full p-1 hover:rounded-lg" : "hover:bg-sky-500 font-medium p-1 rounded-lg text-black"}>About Us</NavLink>
                         <NavLink to="/Events" className={({isActive}) => isActive ? "hover:bg-sky-700 bg-sky-700 font-medium h-full p-1 hover:rounded-lg" : "hover:bg-sky-500 font-medium p-1 rounded-lg text-black"}>Events</NavLink>
                         <NavLink to="/Sign Up" className={({isActive}) => isActive ? "hover:bg-sky-700 bg-red-700 font-medium h-full p-1 hover:rounded-lg" : "hover:bg-sky-500 font-medium p-1 rounded-lg text-sky-600"}>Sign Up</NavLink>
