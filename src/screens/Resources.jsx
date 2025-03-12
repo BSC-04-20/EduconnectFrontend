e451ab4d9f4460597c0c1b0d7ac9bd3441e505de
BSC-04-20 <bsc-04-20@unima.ac.mw>
2025-03-12 10:10:12 +0200

    Responsive Screens

--- src/screens/Resources.jsx
+++ src/screens/Resources.jsx
@@ -6,7 +6,7 @@ export default function Resources(){
     return(
         <div className="flex flex-row gap-5">
             <LectureSideBar/>
-            <section className="ml-[17%] w-[100%]">
+            <section className="mt-[5vh] md:mt-0 ml-[2%] md:ml-[5%] lg:ml-[17%] w-[100%]">
                 <ResourcesHeader/>
                 <ResourcesList/>
             </section>
--- src/screens/StudentDashboard.jsx
+++ src/screens/StudentDashboard.jsx
@@ -4,7 +4,7 @@ export default function StudentDashboard(){
     return(
         <div className="flex flex-row gap-5">
             <StudentSideBar/>
-            <section className="ml-[17%] w-[100%]">
+            <section className="mt-[5vh] md:mt-0 ml-[2%] md:ml-[5%] lg:ml-[17%] w-[100%]">
                 
             </section>
         </div>
