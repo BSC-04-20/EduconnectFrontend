78765f30bf30933c0a50ec281be8182838d75df3
Angellah <bsc-com-22-20@unima.ac.mw>
2025-03-25 23:33:36 +0200

    modified assignmentuploader

--- src/FileUpload.jsx
+++ src/FileUpload.jsx
@@ -3,7 +3,7 @@ import AssignmentUploader from "../src/components/AssignmentUploader"
 export default function FileUpload(){
     return(
         <div>
-            <h1>Hello</h1>
+        
             <AssignmentUploader/>
         </div>
     )
--- src/components/AssignmentUploader.jsx
+++ src/components/AssignmentUploader.jsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from 'react';
 import Uppy from '@uppy/core';
 import Dashboard from '@uppy/react/lib/Dashboard';
 import Tus from '@uppy/tus';
+import { WiCloudUp } from "react-icons/wi";
+// import Upload from "../../../assets/upload.png"
 
 import '@uppy/core/dist/style.min.css';
 import '@uppy/dashboard/dist/style.min.css';
@@ -15,7 +17,28 @@ export default function AssignmentUploader() {
 	// Important: use an initializer function to prevent the state from recreating.
 	const [uppy] = useState(createUppy);
 
-	return <Dashboard theme="light" uppy={uppy} className='bg-blue-900'/>;
+	return (
+
+        <div>
+            <Dashboard theme="light" uppy={uppy} className='h-[80px]'/>
+
+            {/* <div className="flex py-1">
+            <WiCloudUp className="text-sky-900 size-[1.5rem]"  />
+
+            </div> */}
+            
+            <div className="absolute top-10 left-20 h-[35%] ml-20 mr-10">
+                        <img src="/src/assets/upload.png" className="size-full"/>
+            </div>
+            
+         
+            
+
+        </div>
+
+        
+
+    )
 }
 
 
