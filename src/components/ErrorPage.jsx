d23dcb6fc08d8fce061ecfff87a7ad95bab95f6d
Angellah <bsc-com-22-20@unima.ac.mw>
2025-03-26 22:44:56 +0200

    modified 404Page

--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -5,14 +5,14 @@ export default function ErrorPage(){
 	<div className="container flex flex-col items-center justify-center px-5 mx-auto my-8">
 		<div className="max-w-md text-center">
             
-            <div className="mr-28">
-            <span className="text-sky-900"> ERROR</span>
-            </div>
+            <h1 className="mr-28">
+            <p className="text-sky-900 text-2xl"> ERROR</p>
+            </h1>
             
-			<h2 className="mb-8 font-bold text-9xl  text-sky-900">
+			<h2 className=" font-bold text-9xl py-4 text-sky-900">
 				<span className="sr-only">Error</span>404
 			</h2>
-			<p className="text-2xl  md:text-2xl">Sorry, we are unable to find this page.</p>
+			<p className="text-2xl text-sky-900 md:text-2xl mx-8 py-2">Sorry, we are unable to find this page.</p>
 			
 			<button className="px-8 py-4 mt-4 mb-8 bg-sky-900 font-semibold rounded-3xl text-white"> Go to Home </button>
 		</div>
