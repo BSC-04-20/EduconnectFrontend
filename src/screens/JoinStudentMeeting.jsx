a878b3422efb801f5bcae74c25cbdf0f8d09bf14
bsc-com-32-20 <bsc-com-32-20@unima.ac.mw>
2025-04-07 22:46:38 +0200

    Did the primary joining of a meeting from a student

---
+++ src/screens/JoinStudentMeeting.jsx
@@ -0,0 +1,45 @@
+import { JitsiMeeting } from "@jitsi/react-sdk";
+
+
+export default function JoinStudentMeeting() {
+  const roomName = "Room name2";
+  const domain = "meet.jit.si";
+
+  return(
+    <div >
+      <JitsiMeeting
+    domain = {domain}
+    roomName = {roomName}
+    displayName = "Display name"
+    configOverwrite = {{
+      disableProfile: true,
+      enableWelcomePage: false,
+      startWithAudioMuted: true,
+      startWithVideoMuted: true,
+      enableClosePage: true,
+      enableNoisyMicDetection: true,
+      disableAudioLevels: true,
+      disableInviteFunctions: true,
+      disableJoinLeaveNotifications: true,
+      defaultLanguage: 'en',
+      channelLastN: 8,
+      desktopSharingFrameRate: { min: 5, max: 60 }
+    }}
+    interfaceConfigOverwrite={{
+      SHOW_JITSI_WATERMARK: false,
+      SHOW_WATERMARK_FOR_GUESTS: false,
+      HIDE_INVITE_MORE_HEADER: true,
+      DEFAULT_REMOTE_DISPLAY_NAME: 'Participant',
+      DEFAULT_BACKGROUND:"#0c4a6e",
+      TOOLBAR_ALWAYS_VISIBLE: true,
+      DISPLAY_WELCOME_PAGE_CONTENT: false,
+      SHOW_POWERED_BY: false,
+      LANG_DETECTION: true,
+    }}
+    getIFrameRef = { (iframeRef) => { iframeRef.style.height = '585px'; } }
+    userInfo={{ displayName: "Tiko", email: "Email" }}
+    
+/>
+    </div>
+  );
+}
