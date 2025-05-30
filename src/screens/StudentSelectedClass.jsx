69b73ef38f843957b7670268002b2ad4eaa9d4cf
BSC-04-20 <bsc-04-20@unima.ac.mw>
2025-05-30 04:34:26 +0200

    Commented out the student selected class

--- src/screens/StudentSelectedClass.jsx
+++ src/screens/StudentSelectedClass.jsx
@@ -1,16 +1,30 @@
+// Importing component that displays the student's selected class content
 import StudentSelectedClass from "../components/Student/classroom/selected/SelectedClass";
+
+// Importing the sidebar navigation component for students
 import StudentSideBar from "../components/Student/SideBar";
+
+// Importing the top navigation bar component for students
 import StudentTopBar from "../components/Student/TopBar";
 
-// Showing student selected class
-export default function StudentClassScreen(){
-    return(
+// Component responsible for rendering the Student Class screen layout
+export default function StudentClassScreen() {
+    return (
+        // Layout wrapper: using flex to create a horizontal layout with a sidebar and main content
         <div className="flex flex-row gap-5">
-                <StudentSideBar/>
+            
+            {/* Sidebar on the left */}
+            <StudentSideBar />
+
+            {/* Main content area */}
             <section className="mt-[5vh] md:mt-0 ml-[2%] md:ml-[5%] lg:ml-[17%] w-[100%]">
-                <StudentTopBar/>
-                <StudentSelectedClass/>
+                
+                {/* Top navigation bar */}
+                <StudentTopBar />
+                
+                {/* Main class content for the student */}
+                <StudentSelectedClass />
             </section>
         </div>
-    )
+    );
 }
